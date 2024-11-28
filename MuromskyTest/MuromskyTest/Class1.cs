706486267f023f60299e5eac92c1fa6ed31661bd
muromsky-iwmflyb <muromskysss@gmail.com>
2024-11-28 18:29:04 +0900

    project

---
+++ MuromskyTest/MuromskyTest/Class1.cs
@@ -0,0 +1,57 @@
+﻿namespace MuromskyInspectorLib
+{
+    public class FunctionInsp
+    {
+        private string mainInspector = "Васильев Василий Иванович";
+        private string inspectionName = "Автоинспекция г. Чита";
+        private List<string> workers = new List<string>
+        {
+            "Иванов И.И.",
+            "Зиронов Т.А.",
+            "Миронов А.В.",
+            "Васильев В.И."
+        };
+
+        public string GetInspector()
+        {
+            return mainInspector;
+        }
+
+        public string GetCarInspection()
+        {
+            return inspectionName;
+        }
+
+        public bool SetInspector(string fullname)
+        {
+            if (workers.Contains(fullname))
+            {
+                mainInspector = fullname;
+                return true;
+            }
+            return false;
+        }
+
+        public string GenerateNumber(string symbol, int number, string code)
+        {
+            if (string.IsNullOrEmpty(symbol) || char.IsLower(symbol[0]) || !char.IsLetter(symbol[0]))
+            {
+                throw new ArgumentException("Символ должен состоять из одной заглавной буквы.");
+            }
+            return $"{symbol.ToUpper()}{number}_{code}";
+        }
+
+        public List<string> GetWorker()
+        {
+            return new List<string>(workers);
+        }
+
+        public void AddWorker(string fullname)
+        {
+            if (!workers.Contains(fullname))
+            {
+                workers.Add(fullname);
+            }
+        }
+    }
+}
---
+++ MuromskyTest/MuromskyTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using MuromskyInspectorLib;
+
+class Program
+{
+    static void Main()
+    {
+        FunctionInsp insp = new FunctionInsp(); //вызов класса
+
+        Console.WriteLine("Главный инспектор: " + insp.GetInspector()); //тест метода GetInspector
+        Console.WriteLine("Название автоинспекции: " + insp.GetCarInspection()); //тест метода GetCarInspection
+        Console.WriteLine("Главный инспектор: " + insp.SetInspector("Иванов И.И.")); //тест метода SetInspector
+        Console.WriteLine("Главный инспектор: " + insp.GetInspector()); 
+        string generatedNumber = insp.GenerateNumber("A", 1234, "75"); //тест метода GenerateNumber
+        Console.WriteLine("Сгенерированный госномер: " + generatedNumber);
+        Console.WriteLine("Список сотрудников:"); 
+
+        foreach (var worker in insp.GetWorker()) //тест метода GetWorker
+        {
+            Console.WriteLine(worker); //вывод списка
+        }
+
+        insp.AddWorker("Новиков Н.Н."); //тест метода AddWorker
+        Console.WriteLine("Список сотрудников после добавления 'Новиков Н.Н.':");
+        foreach (var worker in insp.GetWorker())
+        {
+            Console.WriteLine(worker); //вывод нового списка
+        }
+    }
+}
